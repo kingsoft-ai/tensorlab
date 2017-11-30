31e828698f7f37614b16bee67a59d5d98131d7c4
CI <ci@CIs-Mac-mini.lan>
2017-11-30 17:32:10 +0800

    add voc loader

--- tensorlab/tools/cv/dataset/loader/voc.py
+++ tensorlab/tools/cv/dataset/loader/voc.py
@@ -13,9 +13,9 @@ class VOCLoder(Loader):
         cfg = config
         self.year = cfg.year
         self.split = cfg.split
-        assert cfg.year in ['07','12'] and cfg.split in ['train','val','test']
-        self.root = os.path.join(root_path, 'VOCdevkit/VOC20%s/' % self.year)
-        self.filelist = 'ImageSets/Main/%s.txt'
+        assert cfg.year in ['07','12'], "wrong year"
+        self.root = root_path
+        self.filelist = 'VOCdevkit/VOC20%s/ImageSets/Main/%s.txt'
         self.seg_filelist = 'VOCdevkit/VOC20%s/ImageSets/Segmentation/%s.txt'
         with open(os.path.join(self.root, self.seg_filelist % (self.year, self.split[0])), 'r') as f:
             self.seg_filenames1 = f.read().split('\n')[:-1]
@@ -30,21 +30,22 @@ class VOCLoder(Loader):
     def read_annotations(self, name):
         bboxes = []
         cats = []
-
-        tree = ET.parse('%sAnnotations/%s.xml' % (self.root, name))
-        root = tree.getroot()
-        width = int(root.find('size/width').text)
-        height = int(root.find('size/height').text)
-        for obj in root.findall('object'):
-            cat = obj.find('name').text
-            cats.append(cat)
-            bbox_tag = obj.find('bndbox')
-            x = int(bbox_tag.find('xmin').text)
-            y = int(bbox_tag.find('ymin').text)
-            w = int(bbox_tag.find('xmax').text)-x
-            h = int(bbox_tag.find('ymax').text)-y
-            bboxes.append([x, y, w, h])
-            # pose = obj.find('post').text
+        # print('%s/VOCdevkit/VOC20%s/Annotations/%s.xml' % (self.root, self.year, name))
+        for i in range(len(self.collect_train_list())):
+            tree = ET.parse('%s/VOCdevkit/VOC20%s/Annotations/%s.xml' % (self.root, self.year, self.collect_train_list()[i]))
+            root = tree.getroot()
+            width = int(root.find('size/width').text)
+            height = int(root.find('size/height').text)
+            for obj in root.findall('object'):
+                cat = obj.find('name').text
+                cats.append(cat)
+                bbox_tag = obj.find('bndbox')
+                x = int(bbox_tag.find('xmin').text)
+                y = int(bbox_tag.find('ymin').text)
+                w = int(bbox_tag.find('xmax').text)-x
+                h = int(bbox_tag.find('ymax').text)-y
+                bboxes.append([x, y, w, h])
+                # pose = obj.find('post').text
 
 
 
@@ -62,38 +63,39 @@ class VOCLoder(Loader):
         return mask_index
 
     def collect_train_list(self):
-        with open(os.path.join(self.root, self.filelist % self.split[0]), 'r') as f:
+        with open(os.path.join(self.root, self.filelist % (self.year, self.split[0])), 'r') as f:
             tra_filenames = f.read().split('\n')[:-1]
             # tra_filenames = 'VOCdevkit/VOC20%s/JPEGImages/%s.jpg' % (self.year, self.tra_filenames)
-        with open(os.path.join(self.root, self.filelist % self.split[1]), 'r') as f:
+        with open(os.path.join(self.root, self.filelist % (self.year, self.split[0])), 'r') as f:
             val_filenames = f.read().split('\n')[:-1]
             # val_filenames = 'VOCdevkit/VOC20%s/JPEGImages/%s.jpg' % (self.year, self.val_filenames)
         # train_files = [tra_filenames, val_filenames]
         train_file = tra_filenames + val_filenames
-        return train_file
+        return train_file[:10]
 
     def collect_test_list(self):
-        with open(os.path.join(self.root, self.filelist % self.split[2]), 'r') as f:
+        with open(os.path.join(self.root, self.filelist % (self.year, self.split[0])), 'r') as f:
             test_filenames = f.read().split('\n')[:-1]
             # test_filenames = 'VOCdevkit/VOC20%s/JPEGImages/%s.jpg' % (self.year, test_filenames)
-        return test_filenames
+        return test_filenames[:10]
 
     def process(self, file_path):
         doc = document.Document()
-        doc.objects = []
+        objects = []
         obj = document.Document()
-        for i, f in enumerate(file_path):
-            with open(os.path.join(self.root, '%sAnnotations/%s.xml' % file_path, 'r')) as f:
-                bboxs, obj_name, w, h= self.read_annotations(f)
-            doc.boundlebox = bboxs
-            doc.image_with = w
-            doc.image_height = h
-            assert len(bboxs) == len(obj_name),  "Wrong lable descriptions"
-            num = len(bboxs)
-            for i in range(num):
-                obj.box = bboxs[i]
-                obj.name = obj_name[i]
-            if obj_name == f:
-                obj.segmentation = self._to_base64(self.read_segmentations(obj_name))
-            doc.objects.append(obj)
 
+        with open(os.path.join(self.root, 'VOCdevkit/VOC20%s/Annotations/%s.xml' % (self.year, file_path)), 'r') as f:
+            bboxs, obj_name, w, h= self.read_annotations(f)
+        doc.width = w
+        doc.height = h
+        assert len(bboxs) == len(obj_name),  "Wrong lable descriptions"
+        # num = len(obj_name)
+        # for i in range(num):
+        obj.box = bboxs
+        obj.name = obj_name
+
+        if obj_name == f:
+            obj.segmentation = self._to_base64(self.read_segmentations(obj_name))
+        objects.append(obj)
+        doc.objects = objects
+        return doc
